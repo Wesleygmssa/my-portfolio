b064fdd30c2eb9d649c3255979b17fdf0b476af4
Bruno <bruno.dev8801@gmail.com>
2022-05-05 22:57:06 -0300

    update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,33 +5,24 @@ import Document, {
     NextScript,
     DocumentContext,
 } from "next/document";
-import { ServerStyleSheet } from "styled-components";
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
-        const sheet = new ServerStyleSheet();
         const originalRenderPage = ctx.renderPage;
 
-        try {
-            ctx.renderPage = () =>
-                originalRenderPage({
-                    enhanceApp: (App) => (props) =>
-                        sheet.collectStyles(<App {...props} />),
-                });
+        // Run the React rendering logic synchronously
+        ctx.renderPage = () =>
+            originalRenderPage({
+                // Useful for wrapping the whole react tree
+                enhanceApp: (App) => App,
+                // Useful for wrapping in a per-page basis
+                enhanceComponent: (Component) => Component,
+            });
 
-            const initialProps = await Document.getInitialProps(ctx);
-            return {
-                ...initialProps,
-                styles: (
-                    <>
-                        {initialProps.styles}
-                        {sheet.getStyleElement()}
-                    </>
-                ),
-            };
-        } finally {
-            sheet.seal();
-        }
+        // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+        const initialProps = await Document.getInitialProps(ctx);
+
+        return initialProps;
     }
 
     render() {
@@ -62,3 +53,5 @@ export default class MyDocument extends Document {
         );
     }
 }
+
+export default MyDocument;
